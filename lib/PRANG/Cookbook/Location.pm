bf205e278c3329982e1631e80f10d3f35dddf80e
Sam Vilain <sam.vilain@catalyst.net.nz>
2010-03-15 17:33:46 +1300

    Cookbook: remove an accidental XML::SRS::Node

--- lib/PRANG/Cookbook/Location.pm
+++ lib/PRANG/Cookbook/Location.pm
@@ -5,6 +5,6 @@ use Moose;
 use PRANG::Graph;
 use PRANG::Cookbook::Role::Location;
 
-with 'PRANG::Cookbook::Role::Location', 'XML::SRS::Node';
+with 'PRANG::Cookbook::Role::Location', 'PRANG::Cookbook::Node';
 
 1;
