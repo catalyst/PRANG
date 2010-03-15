69d1a89013a980e85541890859a8a65943cc8b22
Sam Vilain <sam.vilain@catalyst.net.nz>
2010-03-15 17:33:27 +1300

    Cookbook: all classes must 'use PRANG::Graph'
    Doing that has an important side-effect; the class' metaclass has the
    PRANG::Graph::Meta::Class metarole added to it.

--- lib/PRANG/Cookbook/Date.pm
+++ lib/PRANG/Cookbook/Date.pm
@@ -2,6 +2,7 @@
 package PRANG::Cookbook::Date;
 
 use Moose;
+use PRANG::Graph;
 use PRANG::Cookbook::Role::Date;
 
 with 'PRANG::Cookbook::Role::Date', 'PRANG::Cookbook::Node';
--- lib/PRANG/Cookbook/DateTime.pm
+++ lib/PRANG/Cookbook/DateTime.pm
@@ -2,6 +2,8 @@
 package PRANG::Cookbook::DateTime;
 
 use Moose;
+use PRANG::Graph;
+
 use PRANG::Cookbook::Role::Date;
 use PRANG::Cookbook::Role::Time;
 
